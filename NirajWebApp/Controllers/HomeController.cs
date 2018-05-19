43d393dd5f928c7009c40355d51c5250c55737de
Niraj <niraj_bootcamp@outlook.com>
2018-05-19 13:25:42 -0500

    Add project files.

---
+++ NirajWebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            System.Diagnostics.Trace.WriteLine("Home Page Visited");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Home page of Niraj Athawale";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "You reach me on:";
+
+            return View();
+        }
+    }
+}
